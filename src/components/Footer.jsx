c15b8e57ca82ebdf4e7a8bef7830260bf985c4a8
Deograias <kedrebeogodeogratias@aol.com>
2025-07-25 11:15:34 +0000

    meta tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import logo from '../assets/Logo.svg' // Ensure the path is correct
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <img src={logo} alt="Little Lemon Logo" />
+      <div>
+        <h3>Little Lemon</h3>
+        <p>123 Flavor Street</p>
+        <p>New York, NY, USA</p>
+        <p>Phone: +1 (212) 555-1234</p>
+      </div>
+
+      <div>
+        <h4>Opening Hours</h4>
+        <p>Monday - Saturday: 11 AM - 11 PM</p>
+        <p>Sunday: 12 PM - 10 PM</p>
+      </div>
+
+      <div>
+        <h4>Follow Us</h4>
+        <p>
+          <a href="#">Facebook</a> |
+          <a href="#">Instagram</a> |
+          <a href="#">Twitter</a>
+        </p>
+      </div>
+
+      <p>&copy; 2025 Little Lemon. All rights reserved.</p>
+    </footer>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
+import logo from '../assets/Logo.svg' // Ensure the path is correct
+
 
 const Header = () => {
   return (
     <header>
-        <img src="../assets/Logo.svg" alt="Logo" />
+        <img src={logo} alt="Logo" />
         <Nav />
     </header>
   )
